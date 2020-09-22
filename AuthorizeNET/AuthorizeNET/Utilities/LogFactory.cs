2f3e7189ae480339f1bbe7ee22ccb42d6e5ef518
Doug Reece <doug@rtechnics.com>
2020-09-22 10:18:38 -0600

    Update LogFactory.cs
    Made  change, removing code that caused problems when used with a server-side Blazor website application.

--- AuthorizeNET/AuthorizeNET/Utilities/LogFactory.cs
+++ AuthorizeNET/AuthorizeNET/Utilities/LogFactory.cs
@@ -5,7 +5,11 @@
 
     public static class LogFactory
     {
+#if false
         private static ILoggerFactory LoggerFactory => new LoggerFactory().AddDebug(LogLevel.Debug);
+#else
+        private static ILoggerFactory LoggerFactory => new LoggerFactory(); // Removed due to problem when used with an Blazor web application.
+#endif
 
         public static ILogger getLog(Type classType)
         {
